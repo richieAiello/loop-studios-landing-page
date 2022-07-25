6a47d696bb1065f6c943d72189b5f840b920d818
Richard Aiello <richaiello17@gmail.com>
2022-07-25 13:46:24 -0400

    Updated Hero bg-image path.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,18 @@
 import Nav from './Nav';
 import Hero from './Hero';
+import bgMobile from '../../assets/hero-mobile.jpg';
+import bgDesktop from '../../assets/hero-desktop.jpg';
 
 const Header = props => {
   return (
     <header
-      className="h-[650px] mb-24 text-white
-      bg-[url('./images/mobile/image-hero.jpg')] bg-no-repeat bg-cover 
-      hero:bg-[url('./images/desktop/image-hero.jpg')] hero:bg-center 
-      hero:bg-[rgba(0,0,0,.35)] hero:bg-blend-multiply md:mb-32
-      lg:bg-top lg:mb-40"
+      className={`
+        h-[650px] mb-24 text-white
+        bg-[url('./images/mobile/image-hero.jpg')] bg-no-repeat bg-cover 
+        hero:bg-[url('./images/desktop/image-hero.jpg')] hero:bg-center 
+        hero:bg-[rgba(0,0,0,.35)] hero:bg-blend-multiply md:mb-32
+        lg:bg-top lg:mb-40
+      `}
     >
       <div className="container">
         <Nav />
