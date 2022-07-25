5263b53d715741f73a9ebabd07b118feebf0f60a
Richard Aiello <richaiello17@gmail.com>
2022-07-25 13:24:41 -0400

    Altered hero bg-image.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,8 @@ const Header = props => {
   return (
     <header
       className="h-[650px] mb-24 text-white
-      bg-[url(./images/mobile/image-hero.jpg)] bg-no-repeat bg-cover 
-      hero:bg-[url(./images/desktop/image-hero.jpg)] hero:bg-center 
+      bg-[url('./images/mobile/image-hero.jpg')] bg-no-repeat bg-cover 
+      hero:bg-[url('./images/desktop/image-hero.jpg')] hero:bg-center 
       hero:bg-[rgba(0,0,0,.35)] hero:bg-blend-multiply md:mb-32
       lg:bg-top lg:mb-40"
     >
